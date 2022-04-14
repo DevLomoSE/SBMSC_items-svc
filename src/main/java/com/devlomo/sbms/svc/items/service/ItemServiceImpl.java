7acba5a47f0e2e6b902f35d105318f2cb123c13f
Jonathan Guzman Guadarrama <jonathancggs@gmail.com>
2022-04-14 17:45:16 -0500

    :sparkles: Servie Implementation
    adding the needed code to implement the service described in the interface

--- src/main/java/com/devlomo/sbms/svc/items/service/ItemServiceImpl.java
+++ src/main/java/com/devlomo/sbms/svc/items/service/ItemServiceImpl.java
@@ -1,12 +1,17 @@
 package com.devlomo.sbms.svc.items.service;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.devlomo.sbms.svc.items.model.Item;
+import com.devlomo.sbms.svc.items.model.Product;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -16,14 +21,22 @@ public class ItemServiceImpl implements ItemService {
 
 	@Override
 	public List<Item> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Product> products = Arrays.asList(restClient
+									.getForObject("http://lomopc.com:8001/sbms/products", 
+													Product[].class));
+		return products.stream()
+						.map(p -> new Item(p, 1))
+						.collect(Collectors.toList());
 	}
 
 	@Override
 	public Item findById(Long id, Integer amount) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, String> pathVariables = new HashMap<String, String>();
+		pathVariables.put("id", id.toString());
+		Product product = restClient
+							.getForObject("http://lomopc.com:8001/sbms/products/find?{id}", 
+										Product.class, pathVariables);
+		return new Item(product, amount);
 	}
 
 }
