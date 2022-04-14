153053611db6b61db8bfbf75b670626e9b814c4f
Jonathan Guzman Guadarrama <jonathancggs@gmail.com>
2022-04-13 21:41:45 -0500

    🧍 Model
    adding all properties of the product model

--- src/main/java/com/devlomo/sbms/svc/items/model/Product.java
+++ src/main/java/com/devlomo/sbms/svc/items/model/Product.java
@@ -1,5 +1,15 @@
 package com.devlomo.sbms.svc.items.model;
 
+import java.util.Date;
+
 public class Product {
+	
+	private Long id;
+	
+	private String name;
+	
+	private Double price;
+	
+	private Date createdAt;
 
 }
