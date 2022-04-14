79375138705e99e8bd5084adefb71b3839f63c68
Jonathan Guzman Guadarrama <jonathancggs@gmail.com>
2022-04-13 21:44:52 -0500

    🧍 Model
    adding all properties of the item model

--- src/main/java/com/devlomo/sbms/svc/items/model/Item.java
+++ src/main/java/com/devlomo/sbms/svc/items/model/Item.java
@@ -2,4 +2,31 @@ package com.devlomo.sbms.svc.items.model;
 
 public class Item {
 
+	private Product product;
+	private Integer amount;
+	
+	public Item() {
+		super();
+	}
+
+	public Item(Product product, Integer amount) {
+		super();
+		this.product = product;
+		this.amount = amount;
+	}
+	
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public Integer getAmount() {
+		return amount;
+	}
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+	
+	
 }
