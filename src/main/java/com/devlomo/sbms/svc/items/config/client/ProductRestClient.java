c18e080afe979bdcefc49bb28f8d8ba9b0598778
Jonathan Guzman Guadarrama <jonathancggs@gmail.com>
2022-04-19 11:09:55 -0500

    :link: REST
    refactoring the use of the urls of the products service. using yml file

--- src/main/java/com/devlomo/sbms/svc/items/config/client/ProductRestClient.java
+++ src/main/java/com/devlomo/sbms/svc/items/config/client/ProductRestClient.java
@@ -6,16 +6,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.devlomo.sbms.svc.items.model.Item;
 import com.devlomo.sbms.svc.items.model.RestResponse;
 
-@FeignClient(name = "ms-products-svc", url = "${sbms.api.products.endpoint}${sbms.api.products.resource}")
+@FeignClient("ms-products-svc")
 public interface ProductRestClient {
 	
-	@GetMapping
+	@GetMapping("${sbms.api.products.resource}")
 	public RestResponse findAll();
 	
-	@GetMapping("/find")
+	@GetMapping("${sbms.api.products.resource}/find")
 	public RestResponse findById(@RequestParam Long id);
 
 }
