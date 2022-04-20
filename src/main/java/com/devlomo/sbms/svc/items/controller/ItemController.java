0034231002581e081153ce56805cf337fb625362
Jonathan Guzman Guadarrama <jonathancggs@gmail.com>
2022-04-20 13:17:45 -0500

    :sparkles: Resilience
    settup for resilience and alternative method

--- src/main/java/com/devlomo/sbms/svc/items/controller/ItemController.java
+++ src/main/java/com/devlomo/sbms/svc/items/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.devlomo.sbms.svc.items.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +16,16 @@ import com.devlomo.sbms.svc.items.service.ItemService;
 import lombok.extern.slf4j.Slf4j;
 
 import com.devlomo.sbms.svc.items.model.Item;
+import com.devlomo.sbms.svc.items.model.Product;
 import com.devlomo.sbms.svc.items.model.Response;
 
 @Slf4j
 @RestController
 @RequestMapping("/items")
 public class ItemController {
+	
+	@Autowired
+	private CircuitBreakerFactory cbFactory;
 
 	@Autowired
 	private ItemService itemService;
@@ -44,13 +49,30 @@ public class ItemController {
 											@RequestParam Integer amount){
 		log.info("find item by id: {}, amount: {}", id, amount);
 		try {
-			Item item = itemService.findById(id, amount);
-			return Response.generate("find all", HttpStatus.OK, item);
+			return cbFactory.create("items")
+					.run(() -> {
+						Item item = itemService.findById(id, amount);
+						return Response.generate("find all", HttpStatus.OK, item);
+					}, e -> alternativeMethod(id, amount, e));
 		} catch (Exception e) {
 			log.warn("error while retrieving item by id:{}", id);
 			log.error("error: {}", e);
 			return Response.generate("error", HttpStatus.BAD_REQUEST, e.getMessage());
 		}
+	}
+	
+	public ResponseEntity<Object> alternativeMethod(Long id, Integer amount, Throwable e) {
+		log.error("error from ms: {}", e.getMessage());
+		Item item = new Item();
+		Product product = new Product();
+		
+		item.setAmount(amount);
+		product.setId(id);
+		product.setName("alternative value");
+		product.setPrice(0.0d);
+		item.setProduct(product);
+		
+		return Response.generate("find all", HttpStatus.OK, item);
 	}
 	
 }
