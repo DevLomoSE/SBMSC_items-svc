17f1f0a661942e19601b4333a97a4fa52f085b66
Jonathan Guzman Guadarrama <jonathancggs@gmail.com>
2022-04-20 16:10:24 -0500

    :coffin: Hystrix
    remove unused code because we change hystrix by resilience4j

--- src/main/java/com/devlomo/sbms/svc/items/SbmscItemsSvcApplication.java
+++ src/main/java/com/devlomo/sbms/svc/items/SbmscItemsSvcApplication.java
@@ -2,11 +2,9 @@ package com.devlomo.sbms.svc.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableCircuitBreaker
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
